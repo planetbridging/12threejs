fb29179dbd9caa2d60bdd59e8d68d4c8ca85dac7
planetbridging <47130739+planetbridging@users.noreply.github.com>
2021-09-23 21:41:20 +1000

    button showing editor

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { ChakraProvider } from "@chakra-ui/react"
 
 import MainPage from './pages/mainPage';
 
 function App() {
   return (
     <div className="App">
+      <ChakraProvider>
       <MainPage />
+    </ChakraProvider>
+      
     </div>
   );
 }
--- front/src/pages/mainPage.jsx
+++ front/src/pages/mainPage.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
-
+import { Center,Box,Stack,Text,VStack,StackDivider,Button,ButtonGroup,Checkbox,Wrap,WrapItem,
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton  } from "@chakra-ui/react";
 import App from './planet/planet';
 //import { Editor } from './three.js-dev/editor/js/Editor';
 
@@ -7,16 +14,48 @@ import EditorPage from './editor/editor';
 
 class MainPage extends React.Component {
 
-    state = {
-
+    state ={
+        isOpen: false, onOpen: false, onClose: false
     }
 
     render(){
         return (
             <div className="">
-               <EditorPage />
+               <Button colorScheme="teal" onClick={() =>this.btnOpenDesc()}>
+                    Learn More
+                </Button>
+                {this.renderEditor()}
             </div>
         )
+    }
+
+    btnOpenDesc = () =>{
+        this.setState({isOpen: true});
+    }
+
+    btnCloseDesc = () =>{
+        this.setState({isOpen: false});
+    }
+
+    renderEditor = () =>{
+        const {isOpen, onOpen, onClose } = this.state;
+            return <Drawer
+        isOpen={isOpen}
+        placement="top"
+        size="full"
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <Button onClick={()=>this.btnCloseDesc()}>Back</Button>
+          <DrawerHeader></DrawerHeader>
+          <DrawerBody>
+            <EditorPage />
+          </DrawerBody>
+
+          <DrawerFooter>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     }
 }
 
